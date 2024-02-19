d70db91f4b56278bbd41df92961caabb3ccc9df8
nicola1397 <nicola1397@outlook.it>
2024-02-19 11:39:48 +0100

    feat: created components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script>
 import { store } from "./store.js";
+import AppHeader from "../src/components/AppHeader.vue";
+import AppMain from "../src/components/AppMain.vue";
+import AppFooter from "../src/components/AppFooter.vue";
 
 export default {
   data() {
     return {
       store,
     };
+  },
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
   },
 };
 </script>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<script>
+import { store } from "./store.js";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template></template>
+
+<style scoped></style>
